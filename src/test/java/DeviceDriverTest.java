4bca822d70dd28b0f66b3fbe510efbe0d4ddb873
taylous <taylous99@gmail.com>
2024-04-30 15:50:42 +0900

    [Add] spy를 이용해서 write 검증

--- src/test/java/DeviceDriverTest.java
+++ src/test/java/DeviceDriverTest.java
@@ -1,33 +1,41 @@
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import core.DeviceDriver;
 import devices.DataCenterSSD;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Answers;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class DeviceDriverTest {
 
     @Mock
-    DataCenterSSD mock_DataCenterSSD;
+    DataCenterSSD DATA_CENTER_SSD;
+
+    @Spy
+    DataCenterSSD DATA_CENTER_SSD_SPY;
 
     @Test
     public void 주소에있는_데이터_읽어오기() {
-        DeviceDriver driver = new DeviceDriver(this.mock_DataCenterSSD);
+        DeviceDriver driver = new DeviceDriver(this.DATA_CENTER_SSD);
         byte data = driver.read(0xFF);
         assertEquals(0, data);
     }
 
     @Test
     void 주소에_데이터_쓰기() {
-        DeviceDriver driver = new DeviceDriver(new DataCenterSSD());
-
         long address = 0xFF;
         byte data = 7;
+
+        DeviceDriver driver = new DeviceDriver(this.DATA_CENTER_SSD_SPY);
+        when(driver.read(address)).thenReturn(data);
 
         driver.write(address, data);
 
