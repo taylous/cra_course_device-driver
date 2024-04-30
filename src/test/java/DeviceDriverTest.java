c5a301efddaa8b1a60d3fe2ce6697fd07d0001bd
taylous <taylous99@gmail.com>
2024-04-30 16:02:50 +0900

    [Refactoring] mock을 사용해서 read 테스트 함수 수정

--- src/test/java/DeviceDriverTest.java
+++ src/test/java/DeviceDriverTest.java
@@ -1,46 +1,65 @@
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 import core.DeviceDriver;
 import devices.DataCenterSSD;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Answers;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class DeviceDriverTest {
 
+    public static final int READ_TEST_ITERATION = 5;
+
     @Mock
-    DataCenterSSD DATA_CENTER_SSD;
+    DeviceDriver DEVICE_DRIVER;
 
     @Spy
     DataCenterSSD DATA_CENTER_SSD_SPY;
 
     @Test
     public void 주소에있는_데이터_읽어오기() {
-        DeviceDriver driver = new DeviceDriver(this.DATA_CENTER_SSD);
-        byte data = driver.read(0xFF);
-        assertEquals(0, data);
+        DeviceDriver driver = DEVICE_DRIVER;
+        long address = 0xFF;
+
+        byte expected = 0;
+        byte readData = 0, prevReadData = driver.read(address);
+
+        int iteration = READ_TEST_ITERATION - 1;
+
+        try {
+            while (iteration-- > 0) {
+                Thread.sleep(200);
+                readData = driver.read(address);
+
+                if (prevReadData != readData) {
+                    expected = -1;
+                    break;
+                }
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        verify(driver, times(5)).read(address);
+        assertThat(readData).isEqualTo(expected);
     }
 
     @Test
     void 주소에_데이터_쓰기() {
         long address = 0xFF;
-        byte data = 7;
+        byte actual = 7;
 
         DeviceDriver driver = new DeviceDriver(this.DATA_CENTER_SSD_SPY);
-        when(driver.read(address)).thenReturn(data);
+        when(driver.read(address)).thenReturn(actual);
 
-        driver.write(address, data);
+        driver.write(address, actual);
 
         byte expected = driver.read(0xFF);
 
-        assertThat(7).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
     }
 }
